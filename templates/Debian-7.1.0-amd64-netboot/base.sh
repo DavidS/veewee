5fa8aa45f873593b20d2d6952aea971a167922a5
David Schmitt <david@dasz.at>
2013-08-21 09:10:48 +0200

    Install systemd, mono, use and cleanup local mirror

--- templates/Debian-7.1.0-amd64-netboot/base.sh
+++ templates/Debian-7.1.0-amd64-netboot/base.sh
@@ -1,8 +1,16 @@
+# configure dasz' local mirror
+cp /etc/apt/sources.list{,.orig}
+cat <<EOF >/etc/apt/sources.list
+deb http://kvmhost.dasz:3142/debian wheezy main
+deb http://kvmhost.dasz:3142/security wheezy/updates main
+deb http://kvmhost.dasz:3142/debian wheezy-updates main
+EOF
+
 # Update the box
 apt-get -y update
-apt-get -y install linux-headers-$(uname -r) build-essential
-apt-get -y install zlib1g-dev libssl-dev libreadline-gplv2-dev
-apt-get -y install curl unzip
+apt-get -y install linux-headers-$(uname -r) build-essential \
+		zlib1g-dev libssl-dev libreadline-gplv2-dev \
+		curl unzip systemd mono-complete mono-xsp
 
 # Set up sudo
 echo 'vagrant ALL=NOPASSWD:ALL' > /etc/sudoers.d/vagrant
@@ -19,7 +27,7 @@ GRUB_DEFAULT=0
 GRUB_TIMEOUT=0
 GRUB_DISTRIBUTOR=`lsb_release -i -s 2> /dev/null || echo Debian`
 GRUB_CMDLINE_LINUX_DEFAULT="quiet"
-GRUB_CMDLINE_LINUX="debian-installer=en_US"
+GRUB_CMDLINE_LINUX="init=/lib/systemd/systemd"
 EOF
 
 update-grub
--- templates/Debian-7.1.0-amd64-netboot/cleanup-zetbox.sh
+++ templates/Debian-7.1.0-amd64-netboot/cleanup-zetbox.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-sed -i -e 's/kvmhost.dasz:3142/http.debian.net/g' /etc/apt/sources.list /etc/apt/sources.list.d/*.list
-
+mv /etc/apt/sources.list{.orig,}
 apt-get clean
